6251f15e8a0e484d7dc076c440baa8595c349b5c
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2022-04-17 17:50:19 -0600

    [Add] Get Aeropuertos

---
+++ TECAir_API/Controllers/AeropuertoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TECAir_API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TECAir_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AeropuertoController : ControllerBase
+    {
+        List<Aeropuerto> aeropuertoList = new List<Aeropuerto>();
+
+        // GET: api/<AeropuertoController>
+        [HttpGet]
+        public List<Aeropuerto> Get()
+        {
+            List<Aeropuerto> aeropuertos;
+            using (StreamReader r = new StreamReader("Assets/airports.json"))
+            {
+                string json = r.ReadToEnd();
+                aeropuertos = JsonConvert.DeserializeObject<List<Aeropuerto>>(json);
+            }
+            return aeropuertos;
+        }
+    }
+}
---
+++ TECAir_API/Models/Aeropuerto.cs
@@ -0,0 +1,22 @@
+﻿namespace TECAir_API.Models
+{
+    public class Aeropuerto
+    {
+        public string nombre { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public string codigo { get; set; }
+
+        public Aeropuerto()
+        {
+        }
+
+        public Aeropuerto(string nombre, string ciudad, string pais, string codigo)
+        {
+            this.nombre = nombre;
+            this.ciudad = ciudad;
+            this.pais = pais;
+            this.codigo = codigo;
+        }
+    }
+}
