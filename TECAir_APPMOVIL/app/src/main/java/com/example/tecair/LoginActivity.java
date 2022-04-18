debedb7ec9936de1ecffdebd1f5c6b60a7eebd99
GabrielVL <gavalo.99@gmail.com>
2022-04-17 23:20:55 -0600

    [Add] Register Activity

--- TECAir_APPMOVIL/app/src/main/java/com/example/tecair/LoginActivity.java
+++ TECAir_APPMOVIL/app/src/main/java/com/example/tecair/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,22 +20,34 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText username = (EditText) findViewById(R.id.user);
-        EditText password = (EditText) findViewById(R.id.password);
+        EditText userEditText = (EditText) findViewById(R.id.userEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        TextView registerTextView = findViewById(R.id.registerTextView);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
 
         Button loginbtn = (Button) findViewById(R.id.loginbtn);
-
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("test")) {
-                    user = username.getText().toString();
+                if (userEditText.getText().toString().equals("admin") && passwordEditText.getText().toString().equals("test")) {
+                    user = userEditText.getText().toString();
                     login();
                 } else {
                     Toast.makeText(LoginActivity.this, "Fallo de inicio de sesión", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    public void register() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     public void login() {
--- TECAir_APPMOVIL/app/src/main/java/com/example/tecair/PassengerActivity.java
+++ TECAir_APPMOVIL/app/src/main/java/com/example/tecair/PassengerActivity.java
@@ -1,12 +1,8 @@
 package com.example.tecair;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PassengerActivity extends AppCompatActivity {
 
@@ -15,21 +11,6 @@ public class PassengerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
 
-        CheckBox studentCheckBox = findViewById(R.id.studentCheckBox);
-        EditText uniEditText = findViewById(R.id.uniEditText);
-        EditText idUniEditText = findViewById(R.id.idUniEditText);
 
-        studentCheckBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (studentCheckBox.isChecked()) {
-                    uniEditText.setVisibility(View.VISIBLE);
-                    idUniEditText.setVisibility(View.VISIBLE);
-                } else {
-                    uniEditText.setVisibility(View.INVISIBLE);
-                    idUniEditText.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
     }
 }
