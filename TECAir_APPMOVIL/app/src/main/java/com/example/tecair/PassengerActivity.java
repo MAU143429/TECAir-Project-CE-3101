f06caff0e8191b9002c5a0696c92703a72be37b3
GabrielVL <gavalo.99@gmail.com>
2022-04-17 20:09:56 -0600

    [Update] Student checkbox hides student info

--- TECAir_APPMOVIL/app/src/main/java/com/example/tecair/PassengerActivity.java
+++ TECAir_APPMOVIL/app/src/main/java/com/example/tecair/PassengerActivity.java
@@ -3,6 +3,10 @@ package com.example.tecair;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class PassengerActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class PassengerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
+
+        CheckBox studentCheckBox = findViewById(R.id.studentCheckBox);
+        EditText uniEditText = findViewById(R.id.uniEditText);
+        EditText idUniEditText = findViewById(R.id.idUniEditText);
+
+        studentCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (studentCheckBox.isChecked()) {
+                    uniEditText.setVisibility(View.VISIBLE);
+                    idUniEditText.setVisibility(View.VISIBLE);
+                } else {
+                    uniEditText.setVisibility(View.INVISIBLE);
+                    idUniEditText.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
