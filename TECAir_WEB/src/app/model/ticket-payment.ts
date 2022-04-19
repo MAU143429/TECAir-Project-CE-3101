398398fe74074fb5ea878dc12fcd25210bc2ef88
MAU <mau141226@gmail.com>
2022-04-19 14:19:16 -0600

    [Update] Some comments added

--- TECAir_WEB/src/app/model/ticket-payment.ts
+++ TECAir_WEB/src/app/model/ticket-payment.ts
@@ -1,7 +1,6 @@
 export class TicketPayment {
     dni:number = 0
-    no_reservacion:number = 0
     p_nombre:string = ''
     p_apellido1:string = ''
-    p_apellido2:string = ''
+    p_apellido2:string = '' 
 }
